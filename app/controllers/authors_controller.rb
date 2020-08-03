064410897e227a7b84302ce27b02d04107c7aab0
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-03 15:56:34 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,8 +9,8 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.new(author_params)
     if @author.valid?
-    @author.create!(author_params)
-    redirect_to author_path(@author)
+      @author.create!(author_params)
+      redirect_to author_path(@author)
     else
       render :new
     end
